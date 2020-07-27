6b13b6f308f21de377b492377dc68ed7c740c8cd
Zech Zimmerman <hi@zech.codes>
2020-07-27 19:16:08 -0400

    Create config loaders in the loaders module

--- dippy/config/loaders.py
+++ dippy/config/loaders.py
@@ -1,16 +1,29 @@
 from typing import Any, Dict
+from dippy.config.manager import ConfigLoader
 import pathlib
 
 
-def json_loader(file_path: pathlib.Path) -> Dict[str, Any]:
+# ###   JSON Config Loader   ### #
+
+
+def _json_loader(file_path: pathlib.Path) -> Dict[str, Any]:
     import json
 
     with file_path.open("r") as json_file:
         return json.load(json_file)
 
 
-def yaml_loader(file_path: pathlib.Path) -> Dict[str, Any]:
+json_loader = ConfigLoader("json", r"\.json$", _json_loader)
+
+
+# ###   YAML Config Loader   ### #
+
+
+def _yaml_loader(file_path: pathlib.Path) -> Dict[str, Any]:
     import yaml
 
     with file_path.open("r") as yaml_file:
         return yaml.safe_load(yaml_file)
+
+
+yaml_loader = ConfigLoader("yaml", r"\.ya?ml$", _yaml_loader)
