98aea51faf44b649b31911bbbb15cfae5a155af0
Zech Zimmerman <hi@zech.codes>
2020-07-27 14:13:42 -0400

    Add yaml config loader

--- dippy/config/loaders.py
+++ dippy/config/loaders.py
@@ -8,3 +8,9 @@ def json_loader(file_path: pathlib.Path) -> Dict[str, Any]:
     with file_path.open("r") as json_file:
         return json.load(json_file)
 
+
+def yaml_loader(file_path: pathlib.Path) -> Dict[str, Any]:
+    import yaml
+
+    with file_path.open("r") as yaml_file:
+        return yaml.safe_load(yaml_file)
