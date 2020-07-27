682995cabff467b50172967f81af6296fee9ac9b
Zech Zimmerman <hi@zech.codes>
2020-07-26 21:23:42 -0400

    Add a config file management system

---
+++ tests/test_config.py
@@ -0,0 +1,38 @@
+import dippy.config.config as config
+
+
+class TestConfig:
+    def test_invalid_path(self):
+        try:
+            config.Config(__file__, "config")
+        except Exception as e:
+            assert isinstance(e, config.InvalidConfigPath)
+
+    def test_no_loaders(self):
+        try:
+            c = config.Config(__file__, "")
+            c.load("test_config.py")
+        except Exception as e:
+            assert isinstance(e, config.NoConfigLoaderFound)
+
+    def test_no_config_file(self):
+        try:
+            c = config.Config(__file__, "")
+            c.load("test_config.yaml")
+        except Exception as e:
+            assert isinstance(e, config.InvalidConfigPath)
+
+    def test_load(self):
+        c = config.Config(__file__, "")
+        c.register_loader("test_load", r"\.py$", lambda f: f.name)
+        assert c.load("test_config.py") == "test_config.py"
+
+    def test_load_search(self):
+        c = config.Config(__file__, "")
+        c.register_loader("test_load", r"\.py$", lambda f: f.name)
+        assert c.load("dev_config.py", "test_config.py") == "test_config.py"
+
+    def test_load_search_first(self):
+        c = config.Config(__file__, "")
+        c.register_loader("test_load", r"\.py$", lambda f: f.name)
+        assert c.load("__init__.py", "test_config.py") == "__init__.py"
