155d3276afdf8d648f71e9640ab96bb82f250610
Zech Zimmerman <hi@zech.codes>
2020-12-06 16:42:08 -0500

    Update event tests to work with the async event hub

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,17 +1,19 @@
+from pytest import raises
 from dippy.events import EventHub
+import asyncio
 
 
 def test_event_emit():
     data = "foobar"
     event_data = None
 
-    def listener(event):
+    async def listener(event):
         nonlocal event_data
         event_data = event
 
     hub = EventHub()
     hub.on("testing", listener)
-    hub.emit("testing", data)
+    asyncio.get_event_loop().run_until_complete(hub.emit("testing", data))
 
     assert data == event_data
 
@@ -20,16 +22,16 @@ def test_event_emit_multiple_handlers():
     data = "foobar"
     event_data = []
 
-    def listener_a(event):
+    async def listener_a(event):
         event_data.append("a")
 
-    def listener_b(event):
+    async def listener_b(event):
         event_data.append("b")
 
     hub = EventHub()
     hub.on("testing", listener_a)
     hub.on("testing", listener_b)
-    hub.emit("testing", data)
+    asyncio.get_event_loop().run_until_complete(hub.emit("testing", data))
 
     assert sorted(event_data) == ["a", "b"]
 
@@ -38,16 +40,44 @@ def test_event_stop():
     data = "foobar"
     event_data = []
 
-    def listener_a(event):
+    async def listener_a(event):
         event_data.append("a")
 
-    def listener_b(event):
+    async def listener_b(event):
         event_data.append("b")
 
     hub = EventHub()
     hub.on("testing", listener_a)
     hub.on("testing", listener_b)
     hub.stop("testing", listener_a)
-    hub.emit("testing", data)
+    asyncio.get_event_loop().run_until_complete(hub.emit("testing", data))
 
     assert sorted(event_data) == ["b"]
+
+
+def test_event_stop():
+    data = "foobar"
+    event_data = []
+
+    async def listener_a(event):
+        event_data.append("a")
+
+    async def listener_b(event):
+        event_data.append("b")
+
+    hub = EventHub()
+    hub.on("testing", listener_a)
+    hub.on("testing", listener_b)
+    hub.stop("testing", listener_a)
+    asyncio.get_event_loop().run_until_complete(hub.emit("testing", data))
+
+    assert sorted(event_data) == ["b"]
+
+
+def test_event_register_function():
+    def listener(event):
+        return
+
+    hub = EventHub()
+    with raises(ValueError):
+        hub.on("testing", listener)
