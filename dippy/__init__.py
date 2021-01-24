f8579c7fadf96c38511d5089e243ed54989c7d2d
Zech Zimmerman <hi@zech.codes>
2021-01-24 17:19:52 -0500

    Add component class and event decorator to dippy import

--- dippy/__init__.py
+++ dippy/__init__.py
@@ -1,8 +1,12 @@
-from dippy.bot import Bot
+from dippy.events import event
 import dippy.config as config
+
+# These must be imported at the end to prevent circular imports
+from dippy.components import Component
+from dippy.bot import Bot
 
 
 create = Bot.create
 
 
-__all__ = ["Bot", "config", "create"]
+__all__ = ["Bot", "config", "create", "Component", "event"]
