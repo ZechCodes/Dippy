058f3a2b051d7dd2b5e91aca6d51f6f8b7a7cf7b
Zech Zimmerman <hi@zech.codes>
2021-01-24 12:53:17 -0500

    Test that bot run functions

---
+++ tests/test_bot.py
@@ -0,0 +1,10 @@
+import dippy
+import discord
+import pytest
+
+
+def test_bot_start_up():
+    bot = dippy.Bot.create("Test Bot", __file__)
+
+    with pytest.raises(discord.errors.LoginFailure):
+        bot.run()
