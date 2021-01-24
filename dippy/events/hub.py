5a9e22e6e9ab05d9793c03f3219cd2ecb7627f39
Zech Zimmerman <hi@zech.codes>
2021-01-24 14:13:30 -0500

    Add support for multiple event args

--- dippy/events/hub.py
+++ dippy/events/hub.py
@@ -8,10 +8,10 @@ class EventHub:
     def __init__(self):
         self._handlers: dict[str, set] = defaultdict(set)
 
-    async def emit(self, event_name: str, event_data: Any):
+    async def emit(self, event_name: str, *event_data):
         """Emits an event calling all coroutines that have been registered."""
         for handler in self._handlers.get(event_name, []):
-            await handler(event_data)
+            await handler(*event_data)
 
     def on(self, event_name: str, callback: Callable[[], Coroutine]):
         """Registers a coroutine to listen for an event.
