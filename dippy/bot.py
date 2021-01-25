78800b1e74bf32c41ab9b2224f214d63fc713c6a
Zech Zimmerman <hi@zech.codes>
2021-01-25 17:51:52 -0500

    Add a bunch of discord events

--- dippy/bot.py
+++ dippy/bot.py
@@ -6,6 +6,7 @@ from dippy.events import EventHub
 from dippy.logging import Logging
 from typing import Any, Callable, Dict, Optional, Sequence, Type, Union
 import bevy
+import datetime
 import discord
 import pathlib
 
@@ -71,6 +72,264 @@ class Bot(bevy.Injectable):
                 filter_data,
             )
 
+    async def on_ready(self):
+        await self.events.emit("ready")
+
+    async def on_error(self, event, *args, **kwargs):
+        await self.events.emit(
+            "error", {"event": event, "arguments": args, "keyword_arguments": kwargs}
+        )
+
+    async def on_typing(
+        self,
+        channel: Union[discord.DMChannel, discord.GroupChannel, discord.TextChannel],
+        user: Union[discord.User, discord.Member],
+        when: datetime.datetime,
+    ):
+        data = {"channel": channel, "started": when}
+        event = "member_typing"
+        filter_data = {"channel_id": channel.id}
+        filter_data.update(self._get_user_event_data(user))
+        if isinstance(channel, (discord.GroupChannel, discord.DMChannel)):
+            event = (
+                "group_typing"
+                if isinstance(channel, discord.GroupChannel)
+                else "dm_typing"
+            )
+            data["user"] = user
+        else:
+            data["member"] = user
+
+        await self.events.emit(event, data, filter_data)
+
+    async def on_raw_message_delete(self, payload: discord.RawMessageDeleteEvent):
+        data = {
+            "channel_id": payload.channel_id,
+            "message_id": payload.message_id,
+            "message": payload.cached_message,
+        }
+        event = "chat_message_deleted"
+        if payload.guild_id:
+            event = "message_deleted"
+            data["guild_id"] = payload.guild_id
+
+        filter_data = data.copy()
+        filter_data.pop("message")
+
+        await self.events.emit(event, data, filter_data)
+
+    async def on_raw_bulk_message_delete(
+        self, payload: discord.RawBulkMessageDeleteEvent
+    ):
+        data = {
+            "channel_id": payload.channel_id,
+            "message_ids": payload.message_ids,
+            "messages": payload.cached_messages,
+        }
+        filter_data = {"channel_id": payload.channel_id}
+        event = "chat_messages_bulk_deleted"
+        if payload.guild_id:
+            event = "messages_bulk_deleted"
+            data["guild_id"] = payload.guild_id
+            filter_data["guild_id"] = payload.guild_id
+
+        await self.events.emit(event, data, filter_data)
+
+    async def on_raw_message_edit(self, payload: discord.RawMessageUpdateEvent):
+        channel = self.client.get_channel(payload.channel_id)
+        data = {
+            "channel": channel,
+            "message_id": payload.message_id,
+            "message": payload.cached_message,
+            "data": payload.data,
+        }
+        filter_data = {
+            "channel_id": payload.channel_id,
+            "message_id": payload.message_id,
+        }
+        event = "chat_message_edited"
+        if isinstance(channel, discord.TextChannel):
+            event = "message_edited"
+            data["guild"] = channel.guild
+            filter_data["guild_id"] = channel.guild.id
+
+        await self.events.emit(event, data, filter_data)
+
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        channel = self.client.get_channel(payload.channel_id)
+        data = {
+            "channel": channel,
+            "message_id": payload.message_id,
+            "emoji": payload.emoji,
+        }
+        filter_data = {
+            "channel_id": channel.id,
+            "message_id": payload.message_id,
+            "emoji_ids": {
+                payload.emoji.id,
+            },
+        }
+        event = "chat_reaction_added"
+        if payload.guild_id:
+            event = "reaction_added"
+            data["guild"] = self.client.get_guild(payload.guild_id)
+            data["member"] = payload.member
+            filter_data["guild_id"] = payload.guild_id
+            filter_data.update(
+                self._get_user_event_data(data["guild"].get_member(payload.user_id))
+            )
+        else:
+            data["user"] = self.client.get_user(payload.user_id)
+            filter_data.update(
+                self._get_user_event_data(self.client.get_user(payload.user_id))
+            )
+
+        await self.events.emit(event, data, filter_data)
+
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+        channel = self.client.get_channel(payload.channel_id)
+        data = {
+            "channel": channel,
+            "message_id": payload.message_id,
+            "emoji": payload.emoji,
+        }
+        filter_data = {
+            "channel_id": channel.id,
+            "message_id": payload.message_id,
+            "emoji_ids": {
+                payload.emoji.id,
+            },
+        }
+        event = "chat_reaction_added"
+        if payload.guild_id:
+            event = "reaction_added"
+            data["guild"] = self.client.get_guild(payload.guild_id)
+            data["member"] = payload.member
+            filter_data.update(
+                self._get_user_event_data(data["guild"].get_member(payload.user_id))
+            )
+        else:
+            data["user"] = self.client.get_user(payload.user_id)
+            filter_data.update(
+                self._get_user_event_data(self.client.get_user(payload.user_id))
+            )
+
+        await self.events.emit(event, data, filter_data)
+
+    async def on_raw_reaction_clear_emoji(
+        self, payload: discord.RawReactionClearEmojiEvent
+    ):
+        channel = self.client.get_channel(payload.channel_id)
+        data = {
+            "channel": channel,
+            "message_id": payload.message_id,
+            "emoji": payload.emoji,
+        }
+        filter_data = {
+            "channel_id": channel.id,
+            "message_id": payload.message_id,
+            "emoji_ids": {
+                payload.emoji.id,
+            },
+        }
+        event = "chat_reactions_emoji_cleared"
+        if payload.guild_id:
+            event = "reactions_emoji_cleared"
+            data["guild"] = self.client.get_guild(payload.guild_id)
+            filter_data["guild_id"] = payload.guild_id
+
+        await self.events.emit(event, data, filter_data)
+
+    async def on_raw_reaction_clear(self, payload: discord.RawReactionClearEvent):
+        channel = self.client.get_channel(payload.channel_id)
+        data = {
+            "channel": channel,
+            "message_id": payload.message_id,
+        }
+        filter_data = {"channel_id": channel.id, "message_id": payload.message_id}
+        event = "chat_reactions_cleared"
+        if payload.guild_id:
+            event = "reactions_cleared"
+            data["guild"] = self.client.get_guild(payload.guild_id)
+            filter_data["guild_id"] = payload.guild_id
+
+        await self.events.emit(event, data, filter_data)
+
+    async def on_member_join(self, member: discord.Member):
+        await self.events.emit(
+            "member_joined",
+            {"member": member, "guild": member.guild},
+            self._get_user_event_data(member),
+        )
+
+    async def on_member_remove(self, member: discord.Member):
+        await self.events.emit(
+            "member_left",
+            {"member": member, "guild": member.guild},
+            self._get_user_event_data(member),
+        )
+
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        data = {
+            "guild": after.guild,
+            "member": after,
+        }
+        filter_data = self._get_user_event_data(after)
+
+        if before.status != after.status:
+            _data = data.copy()
+            _data["status"] = after.status
+            _data["previous"] = before.status
+            await self.events.emit("member_status_changed", _data, filter_data)
+
+        if before.activity != after.activity:
+            _data = data.copy()
+            _data["activity"] = after.activity
+            _data["previous"] = before.activity
+            await self.events.emit("member_activity_changed", _data, filter_data)
+
+        if before.nick != after.nick:
+            _data = data.copy()
+            _data["nickname"] = after.nick
+            _data["previous"] = before.nick
+            await self.events.emit("member_nickname_changed", _data, filter_data)
+
+        if before.roles != after.roles:
+            _data = data.copy()
+            _data["roles"] = after.roles
+            _data["previous"] = before.roles
+            await self.events.emit("member_roles_changed", _data, filter_data)
+
+        if before.pending != after.pending:
+            _data = data.copy()
+            _data["pending"] = after.pending
+            _data["previous"] = before.pending
+            await self.events.emit("member_roles_changed", _data, filter_data)
+
+    async def on_user_update(self, before: discord.User, after: discord.User):
+        data = {
+            "user": after,
+        }
+        filter_data = self._get_user_event_data(after)
+
+        if before.avatar != after.avatar:
+            _data = data.copy()
+            _data["avatar"] = after.avatar
+            _data["previous"] = before.avatar
+            await self.events.emit("user_avatar_changed", _data, filter_data)
+
+        if before.name != after.name:
+            _data = data.copy()
+            _data["name"] = after.name
+            _data["previous"] = before.name
+            await self.events.emit("user_name_changed", _data, filter_data)
+
+        if before.discriminator != after.discriminator:
+            _data = data.copy()
+            _data["discriminator"] = after.discriminator
+            _data["previous"] = before.discriminator
+            await self.events.emit("user_discriminator_changed", _data, filter_data)
+
     def _get_user_event_data(
         self, user: Union[discord.Member, discord.User]
     ) -> Dict[str, Any]:
@@ -89,7 +348,7 @@ class Bot(bevy.Injectable):
             data["guild_id"] = message.guild.id
 
         if not ignore_reactions:
-            data["reaction_ids"] = set(reaction.id for reaction in message.reactions)
+            data["emoji_ids"] = set(reaction.emoji.id for reaction in message.reactions)
 
         return data
 
@@ -101,7 +360,21 @@ class Bot(bevy.Injectable):
             if hasattr(self.client, "listen")
             else self.client.event
         )
+        register(self.on_error)
+        register(self.on_member_join)
+        register(self.on_member_remove)
+        register(self.on_member_update)
         register(self.on_message)
+        register(self.on_raw_bulk_message_delete)
+        register(self.on_raw_message_delete)
+        register(self.on_raw_message_edit)
+        register(self.on_raw_reaction_add)
+        register(self.on_raw_reaction_clear)
+        register(self.on_raw_reaction_clear_emoji)
+        register(self.on_raw_reaction_remove)
+        register(self.on_ready)
+        register(self.on_typing)
+        register(self.on_user_update)
 
     def _tidy_app_path(self, path: Union[pathlib.Path, str]) -> pathlib.Path:
         tidy_path = pathlib.Path(path)
