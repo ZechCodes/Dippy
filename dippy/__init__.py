cece35aa30893ca0986dce0d80061ed3a9e3310a
Zech Zimmerman <hi@zech.codes>
2021-01-25 16:04:32 -0500

    Import filters into dippy for ease of access

--- dippy/__init__.py
+++ dippy/__init__.py
@@ -1,13 +1,39 @@
 from dippy.events import event
 import dippy.config as config
-from dippy.filters import filters
+from dippy.filters.filters import (
+    BaseFilter as Filter,
+    ChannelFilter,
+    GlobalFilter,
+    RoleFilter,
+    UserFilter,
+    LabelFilter,
+    GuildFilter,
+    ReactionFilter,
+    MessageFilter,
+)
 
 # These must be imported at the end to prevent circular imports
-from dippy.components import Component
+from dippy.components import Component, filters
 from dippy.bot import Bot
 
 
 create = Bot.create
 
 
-__all__ = ["Bot", "config", "create", "Component", "event", "filters"]
+__all__ = [
+    "Bot",
+    "ChannelFilter",
+    "Component",
+    "Filter",
+    "GlobalFilter",
+    "GuildFilter",
+    "LabelFilter",
+    "MessageFilter",
+    "ReactionFilter",
+    "RoleFilter",
+    "UserFilter",
+    "config",
+    "create",
+    "event",
+    "filters",
+]
