32759e3ed2c41e83a7d6848023262acefa6a607b
Zech Zimmerman <hi@zech.codes>
2021-01-25 14:42:21 -0500

    Make filters more accessible

--- dippy/__init__.py
+++ dippy/__init__.py
@@ -1,5 +1,6 @@
 from dippy.events import event
 import dippy.config as config
+from dippy.filters import filters
 
 # These must be imported at the end to prevent circular imports
 from dippy.components import Component
@@ -9,4 +10,4 @@ from dippy.bot import Bot
 create = Bot.create
 
 
-__all__ = ["Bot", "config", "create", "Component", "event"]
+__all__ = ["Bot", "config", "create", "Component", "event", "filters"]
