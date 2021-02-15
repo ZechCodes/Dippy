49de4a56669cbaa9056cc004c4fe8ff84ca869ad
Zech Zimmerman <hi@zech.codes>
2021-02-14 21:48:13 -0500

    Create a barebones client

---
+++ dippy/client.py
@@ -0,0 +1,30 @@
+from bevy import Factory, Injectable
+from dippy.events import EventHub
+from dippy.extensions import ExtensionManager
+from dippy.logging import Logging
+from discord.ext.commands import Bot
+
+
+class Client(Bot, Injectable):
+    log_factory: Factory[Logging]
+    events: EventHub
+    extension_manager: ExtensionManager
+
+    def __init__(self, name: str = "Dippy.bot", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.log = self.log_factory(name)
+        Logging.setup_logger()
+        self.extension_manager.load_extensions()
+        self.extension_manager.create_extensions()
+
+    def dispatch(self, event_name, *args, **kwargs):
+        super(Client, self).dispatch(event_name, *args, **kwargs)
+        self.events.emit(event_name, *args, *kwargs)
+
+    async def on_ready(self):
+        self.log.info("Bot is ready")
+
+    @classmethod
+    def launch(cls, token: str = None, *args, **kwargs):
+        client = cls(*args, **kwargs)
+        client.run(token)
