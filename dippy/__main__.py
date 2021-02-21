05cda3fc0daa22eafaaac94b008327c19c44f62e
Zech Zimmerman <hi@zech.codes>
2021-02-21 15:30:16 -0500

    Fallback to using envar for token

--- dippy/__main__.py
+++ dippy/__main__.py
@@ -1,10 +1,19 @@
 from dippy import Client
 import argparse
+import os
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="Launch a Dippy bot for Discord")
-    parser.add_argument("--token", "-t", help="The token to use for the bot")
+    parser.add_argument(
+        "--token", "-t", help="The token to use for the bot", default=None
+    )
+    parser.add_argument(
+        "--tokenvar",
+        "-v",
+        help="The environment variable that stores the token to use for the bot",
+        default="DISCORD_TOKEN",
+    )
     parser.add_argument("--name", "-n", help="Name for the bot")
     parser.add_argument(
         "--prefix", "-p", help="The command prefix to use for bot commands", default="!"
@@ -14,4 +23,5 @@ def get_args():
 
 
 args = get_args()
-Client.launch(token=args.token, command_prefix=args.prefix, name=args.name)
+token = args.token if args.token else os.getenv(args.tokenvar)
+Client.launch(token=token, command_prefix=args.prefix, name=args.name)
