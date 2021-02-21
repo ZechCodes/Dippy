13c954f1fbc0defc12c8d43445ebbf1f21e6616d
Zech Zimmerman <hi@zech.codes>
2021-02-21 15:29:50 -0500

    Set default prefix

--- dippy/__main__.py
+++ dippy/__main__.py
@@ -7,7 +7,7 @@ def get_args():
     parser.add_argument("--token", "-t", help="The token to use for the bot")
     parser.add_argument("--name", "-n", help="Name for the bot")
     parser.add_argument(
-        "--prefix", "-p", help="The command prefix to use for bot commands"
+        "--prefix", "-p", help="The command prefix to use for bot commands", default="!"
     )
 
     return parser.parse_args()
